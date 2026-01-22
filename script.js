98bfdc0f0ab4d259cf0c68535397880ff17f5f5a
Peter William <peter.william@estudante.ufjf.br>
2026-01-22 10:24:57 -0300

    checkpoint feito

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let Jogador = {
+const areaMapa = document.querySelector('#area');
+
+const Jogador = {
     y: 0,
     x: 0,
     andarDireita : function (){
@@ -6,6 +8,31 @@ let Jogador = {
     }
 
 }
+const Mapa = {
+    tam: 50,
+    checkpoit:47 ,
+    area: areaMapa,
+    desenharMapa: function () {
+        areaMapa.innerHTML = "";
+        for (let i = 0; i < Mapa.tam; i++) {
+                const celula = document.createElement("span");
+                celula.classList.add("celula");
+                celula.id = String(i);
+                areaMapa.appendChild(celula);
+        }
+        document.getElementById("47").style.backgroundImage ="url(https://i.pinimg.com/736x/7a/25/27/7a2527042f10403dbbe74146e93640b7.jpg)";
+    }
+    
+}
 
-const botAndarD = document.getElementById("botD");
-botAndarD.addEventListener("click", Jogador.andarDireita() );
+
+
+
+
+// const botAndarD = document.getElementById("botD");
+// botAndarD.addEventListener("click", Jogador.andarDireita() );
+
+
+
+// "FUNCAO" principal do codigo
+Mapa.desenharMapa();
